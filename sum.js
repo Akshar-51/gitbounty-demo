dbd7bef5ab7fda2a889ae9ac63b4ef9e20ccca91
Fajar <jossfajar21@gmail.com>
2026-08-08 00:06:53 +0700

    fix: sum() skips the first element (#1) (#2)

--- sum.js
+++ sum.js
@@ -1,7 +1,7 @@
 // Sums an array of numbers.
 function sum(numbers) {
   let total = 0;
-  for (let i = 1; i < numbers.length; i++) {
+  for (let i = 0; i < numbers.length; i++) {
     total += numbers[i];
   }
   return total;
